a0eb7b5c4a5f0213acce8ad6358d9501b33d442f
Pop Dávid <pop.david.tibor@students.jedlik.eu>
2023-10-18 09:13:03 +0200

    merge conflikt kez

--- variables.py
+++ variables.py
@@ -1,6 +1,33 @@
-UCweapons = {
-    'fakard (1)': 1,     #1-es szintű kard'  
-    'Márványkard (4)': 4  #3-szintu kard 
+from classes import *
+fakard = weapon('fakard (1)', 1, 0)
+marvanykard = weapon(name='márványkard (3)', damage = 3, price=10)
+vaskard = weapon(name='vaskard (6)', damage=6, price=25)
+obszidianel = weapon(name='obszidiánél (11)', damage=11, price=55)
+
+fapancel = armor(name='fapáncél (1)', shield=1, price=25)
+marvanypancel = armor(name='marvanypáncél (3)', shield=3, )
+
+WEAPON = [
+    fakard,
+    marvanykard,
+    vaskard,
+    obszidianel
+]
+
+ARMOR = [
+    
+]
+
+
+UCarmor = [
+
+]
+
+
+
+UCshield = [
+
+]
 
 
 Cshield = [

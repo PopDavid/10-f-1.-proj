266d3fccbdf3fc01deb8d0c6ad835e8c8002fb88
Pop Dávid <pop.david.tibor@students.jedlik.eu>
2023-11-08 10:22:51 +0100

    bossinfo elkezdése

---
+++ html/script.js
@@ -0,0 +1,30 @@
+function bossinfo(boss)
+{
+    value = isvisible(boss)
+    console.log(value)
+    if (value)
+    {
+        document.getElementById(boss).style.visibility = 'hidden';
+    }
+    else
+    {
+        document.getElementById(boss).style.visibility = 'visible';
+    }
+    console.log(isvisible(boss));
+
+}
+
+
+
+function isvisible(elem){
+    let style = document.getElementById(elem)
+    console.log(style.style.visibility)
+    if (style.style.visibility == 'visible'){
+        console.log('visible')
+        return true;
+    } 
+    else{
+        console.log('hidden')
+        return false
+    }
+}
